82fc8a4da095ad8b3dde7bc91f58f223a08436b7
aaronrss <A01335324@itesm.mx>
2021-12-02 18:40:30 -0600

    AVANCE 1.5

--- maxhel.exs
+++ maxhel.exs
@@ -17,7 +17,9 @@ defmodule Maxhel do
         filename
         |> File.stream!()
         |> Enum.map(&String.trim/1)
+        |> encryptC()
         # LO DE ARRIBA PARECE SER SOLUCION
+        # LO DE ABAJO HACE UNA LISTA DE LISTAS
         #|> Enum.map(&(String.split(&1,",")))
         #|> encryptC()
         # Cuenta el numero de renglos para dividir la encripción a los procesadores
@@ -25,32 +27,40 @@ defmodule Maxhel do
         #|> IO.inspect()
     end
 
-    def encryptFormula(list)
-        
+    def caesar(list, n) do
+        Enum.map list, &(perform_addition(&1, n))
+        |> to_charlist
+        |> List.to_string
+    end
 
+    defp perform_addition(char_val, n) when char_val < 122 do
+        char_val + n
+    end
+
+    defp perform_addition(_, n) do
+        97 + n
     end
 
     @doc """
     Encrypt the contents of the file with Caesar Cipher
     """
-    def encryptC(mapfile) do, do: do_encryptC(mapfile, [])
+    def encryptC(mapfile), do: do_encryptC(mapfile, [])
     defp do_encryptC([], result),
+        do: result
     defp do_encryptC([head|tail], result),
-    # PARECE SER SOLUCION PERO SEGUIMOS IMPLEMENTADO FORMAS
-        do: do_encryptC(tail, result ++ encryptFormula(String.graphemes(Enum.join(head)))
-       # Recorrer renglon 
-       # new_data = for row <- file do
-    end
+        do: do_encryptC(tail, result ++ caesar(to_charlist(head), 0))
+    defp do_encryptC([""|tail], result),
+        do: do_encryptC(tail, result ++ caesar(to_charlist()))
 
     #PARECE SER TODO LO NECESARIO PARA CONCURRENCIA
-    def encrypt_parallel(n, cores) do
-        block = div(n, cores) #divides the range in blocks
+    #def encrypt_parallel(n, cores) do
+    #   block = div(n, cores) #divides the range in blocks
 
-        1..cores
-        |> Enum.map(&Task.async(fn -> encryptC((&1 * block), ((&1 - 1) * block + 1)) end))
-        |> Enum.map(&Task.await(&1, 50000))
-        |> Enum.sum()
-    end
+#        1..cores
+ #       |> Enum.map(&Task.async(fn -> encryptC((&1 * block), ((&1 - 1) * block + 1)) end))
+  #      |> Enum.map(&Task.await(&1, 50000))
+   #     |> Enum.sum()
+    #end
 
     @doc """
     Write the content encrypted into a file
@@ -64,7 +74,7 @@ defmodule Maxhel do
     """
     def main() do
         filename = IO.gets("Give me the name of the file with the extension: ")
-            |> String.replace("\n", "")
+            |> String.replace("\n", "10")
         data = filename
             |> read_data()
         
